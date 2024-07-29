8fb108986e17d99c0304e5002a3773db00275a43
LSpace <tbao12901@gmail.com>
2024-07-29 15:37:19 +0700

    Update Main.php

--- src/lenlenlL6/caughttlodestone/Main.php
+++ src/lenlenlL6/caughttlodestone/Main.php
@@ -4,9 +4,12 @@ namespace lenlenlL6\caughttlodestone;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\Config;
+use pocketmine\utils\SingletonTrait;
 
 class Main extends PluginBase {
 
+    use SingletonTrait;
+    
     public function onEnable() : void {
         
     }
