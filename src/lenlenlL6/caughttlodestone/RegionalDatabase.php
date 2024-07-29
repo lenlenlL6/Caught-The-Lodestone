f672e6e3396ec68a7a72d35853d61601eb9a822f
LSpace <tbao12901@gmail.com>
2024-07-29 20:33:16 +0700

    Create RegionalDatabase.php

---
+++ src/lenlenlL6/caughttlodestone/RegionalDatabase.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace lenlenlL6\caughttlodestone;
+
+class RegionalDatabase {
+
+  public static array $setRegion = [];
+  
+  public static array $editRegion = [];
+  
+  public static array $region = [];
+}
