e6662ca0b3c187738b883823fbefb5c82fa5e59f
LSpace <tbao12901@gmail.com>
2024-07-29 20:51:45 +0700

    Update CoaughtTheLodestoneEditCommand.php

--- src/lenlenlL6/caughttlodestone/command/CoaughtTheLodestoneEditCommand.php
+++ src/lenlenlL6/caughttlodestone/command/CoaughtTheLodestoneEditCommand.php
@@ -11,14 +11,14 @@ use pocketmine\Server;
 
 class CoaughtTheLodestoneEditCommand extends Command {
 
-  public function __construct(){
+    public function __construct(){
         parent::__construct("ctle", "edit Caught The Lodestone", "");
         $this->setPermission("ctl.command.edit");
     }
 
     public function execute(CommandSender $sender, string $commandLabel, array $args): bool{
         if($sender instanceof Player){
-            if(isset($args[0]) {
+            if(isset($args[0])) {
                 switch($args[0]) {
                     case "set":
                          
